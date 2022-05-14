0cb04dda5ef7d651bd4147ce8a9a14e27e498b91
Michel dos Santos Kuguio <michel.kuguio@gmail.com>
2022-05-14 10:35:55 -0300

    feat: add todo list

--- AppTestBases/App.tsx
+++ AppTestBases/App.tsx
@@ -1,29 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Switch } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {StyleSheet, Text, View, Switch} from 'react-native';
 import {EstadoLocal} from "./src/pages/EstadoLocal";
 import {useState} from "react";
 import {CicloDeVida} from "./src/pages/CicloDeVida";
+import {ToDoList} from "./src/pages/ToDoList";
 
 export default function App() {
-  const [mount, setMount] = useState<boolean>(false)
-  const invertMount = ()=>setMount(current=>!current)
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="light" backgroundColor={'#f00'} />
-      <EstadoLocal />
-      <Text>Montar Pagina Ciclo de Vida</Text>
-      <Switch value={mount} onChange={invertMount}/>
-      {mount&&(<CicloDeVida />)}
-    </View>
-  );
+    const [mount, setMount] = useState<boolean>(false)
+    const invertMount = () => setMount(current => !current)
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+            <StatusBar style="light" backgroundColor={'#f00'}/>
+            <EstadoLocal/>
+            <Text>Montar Pagina Ciclo de Vida</Text>
+            <Switch value={mount} onChange={invertMount}/>
+            {mount && (<CicloDeVida/>)}
+            <ToDoList titulo={'teste'}/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
 });
